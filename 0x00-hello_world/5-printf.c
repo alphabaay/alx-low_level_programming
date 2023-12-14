44387f1da0c60870bc6f4b176fbcf433e5c66c5d
alphabaay <alphabaay@gmail.com>
2023-12-14 03:48:36 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#includeb <stdio.h>
+
+/**
+ * main - prints "with proper gramer,but the outcome is
+ * a piece of art," followed newline
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+
+{
+    printf("with proper gramer, but the outcome is piece of art,\n");
+    return (0);
+}
+
+
+
+
