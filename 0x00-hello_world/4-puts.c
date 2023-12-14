fb3476902b08d761e8a9f9cdc340628a96157dbf
alphabaay <alphabaay@gmail.com>
2023-12-14 03:29:17 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  * main . prints "Programming is like building a multilingual
  * puzzle, followed by a new line.
- *Return: Always 0.
+ * Return: Always 0.
  */
 
 int main(void)
 
 {
    puts("\"Programming is like building a multilingual puzzle");
-   Return(0);
+   return(0);
 }
