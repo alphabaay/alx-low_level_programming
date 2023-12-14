d3697eaea795df2d9a773936485d379a28cd3059
alphabaay <alphabaay@gmail.com>
2023-12-14 07:29:09 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints the size of various types based on
+ * the computer
+ * Return : Always 0.
+ */
+ 
+int main(void)
+
+{ 
+  printf("size of char: %zu byte(s)\n", sizeof(char));
+  
+  printf("size of an int: %zu byte(s)\n", sizeof(int));
+  printf("size of a long int: %zu byte(s)\n",sizeof(long int));
+  printf("size of a long long int: %zu byte(s)\n",sizeof(long long int));
+  printf("size of a float % zu byte(s)\n", sizeof(float));
+  return (0);
+}
+
+
