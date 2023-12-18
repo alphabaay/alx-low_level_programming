dd8fe868fc8f12b1539b64b8cf2f730d4b1aca0d
alphabaay <alphabaay@gmail.com>
2023-12-18 12:20:53 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 5-print_numbers.c -o 5-print_number

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+
+{
+   
+      char low;
+
+      for (low = 'a'; low <= 'z'; low++)
+      putchar(low);
+
+      for (low = 'A'; low <= 'z'; low++)
+      putchar(low);
+ 
+ 
+      putchar('\n');
+   
+      return (0);
