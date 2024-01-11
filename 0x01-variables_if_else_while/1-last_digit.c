eeba1628f19a94f85f8caa75be6816e2032327da
alphabaay <alphabaay@gmail.com>
2024-01-11 00:35:21 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,9 +23,9 @@ int main(void)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	}
 	else
-	{
+{
 		printf("Last digit of %d is %d and is 0\n", n, m);
 	}
 	return (0);
 }
-}
+
