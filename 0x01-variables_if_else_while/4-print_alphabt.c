ed52037feb94268ece0d69e01881bba75fa4c216
alphabaay <alphabaay@gmail.com>
2024-01-11 00:49:20 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 
 
-
- /**
-  * main - entry point
-  * Return: Always 0 (success)
-  */
+/**
+ * main - program that prints the alphabet in lowercase and upper case
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
-
 {
-    
-	  char low, e, q;
+	char letter;
 
-	  e = 'e';
-	  q = 'q';
+	for (letter = 'a'; letter <= 'z'; letter++)
 
-          for (low = 'a'; low <= 'z'; low++)
-           {
-	   if (low != e && low != q)
-	   putchar(low);
-	   {
-            putchar('\n');
-	    return (0);
+	{
+	if (letter != 'e' && letter != 'q')
+	putchar(letter);
+	}
 
+	putchar('\n');
+
+	return (0);
 }
