afab11d467bc07a21822a8c5d6bcdb09f6e90349
alphabaay <alphabaay@gmail.com>
2024-01-11 00:51:29 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 
-#include <stdlib.h>
-
-#include <time.h>
 
 /**
- * main -Entry point 
- * return: Always 0 (success)
+ * main - program that prints the alphabet in lowercase and upper case
+ *
+ * Return: Always 0.
  */
 
 int main(void)
-
-
 {
-   
-       int i;
+	char letter;
 
-       for  (i = 0; i <10; i++>)
-       printf("%d", i); 
-       printf("\n");   
-       
-       return (0);    
+	for (letter = 'a'; letter <= 'z'; letter++)
 
+	{
+	if (letter != 'e' && letter != 'q')
+	putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
 }
