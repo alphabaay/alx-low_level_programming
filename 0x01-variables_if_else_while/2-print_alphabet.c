6135e77b23e1400c487cd55644706d9ea263b50e
alphabaay <alphabaay@gmail.com>
2024-01-11 00:38:04 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,17 +2,17 @@
 
 
 /**
- * main -prints the alphabetic
- * Return: Always (Succes)
+ * main - program that prints the alphabet in lowercase
+ *
+ *  Return: Always 0.
  */
-
 int main(void)
-
 {
-      char c;
-      for (c = 'a'; c <= 'z'; c++)
-      putchar(c);
-      putchar('\n');
-      return (0);
+	char letter;
 
+	for (letter = 'a'; letter <= 'z'; letter++)
+	putchar(letter);
+	putchar('\n');
+	return (0);
 }
+
